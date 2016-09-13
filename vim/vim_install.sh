e03f4992f32f78f3c41b9e87f741b8520d04fd4f
kong_ling_chun <kong_ling_chun@sina.com>
2016-09-13 17:01:24 +0800

    install curl before install vim

--- vim/vim_install.sh
+++ vim/vim_install.sh
@@ -1,5 +1,6 @@
 sudo apt-get remove vim-common
 sudo apt-get install vim vim-doc vim-scripts vim-runtime exuberant-ctags
+sudo apt-get install curl
 
 curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
 cp vimrc.local ~/.vimrc.local
