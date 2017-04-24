30e1571639c1cf104e598b67b83597dc91b44333
konglingchun <kong_ling_chun@sina.com>
2017-04-24 09:23:39 +0800

    Update gcc_g++_version.sh
    update gcc g++ version

--- gcc_g++_version.sh
+++ gcc_g++_version.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-	sudo apt-get install gcc
-	sudo apt-get install g++
-	sudo apt-get install gcc-4.4 gcc-4.4-multilib g++-4.4 g++-4.4-multilib
-	sudo apt-get install gcc-4.6 gcc-4.6-multilib g++-4.6 g++-4.6-multilib
-	sudo apt-get install gcc-4.8 gcc-4.8-multilib g++-4.8 g++-4.8-multilib
-	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.4 100
-	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 80
+	sudo apt-get install gcc-4.8
+	sudo apt-get install g++-4.8
+	sudo apt-get install gcc-4.8-multilib
+	sudo apt-get install g++-4.8-multilib
 	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
-	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.4 100
-	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.6 80
-	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
-	sudo update-alternatives --install /usr/bin/cpp cpp-bin /usr/bin/cpp-4.4 100
-	sudo update-alternatives --install /usr/bin/cpp cpp-bin /usr/bin/cpp-4.6 80
-	sudo update-alternatives --install /usr/bin/cpp cpp-bin /usr/bin/cpp-4.8 50
+	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50	
+	sudo apt-get install gcc-5
+	sudo apt-get install g++-5
+	sudo apt-get install gcc-5-multilib
+	sudo apt-get install g++-5-multilib
+	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 100
+	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 100
 
 #查看版本
 	#gcc -v
