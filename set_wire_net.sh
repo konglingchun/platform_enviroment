19adeb8f4851a16382177afe239f20be0daee467
konglingchun <kong_ling_chun@sina.com>
2017-06-27 13:16:44 +0800

    Create set_wire_net.sh
    add ethernet card configure

---
+++ set_wire_net.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#IP configration
+
+DHCP=0
+IP_PRE=192.168.101
+
+#SET REMOTE IP
+REMOTE_IP=$IP_PRE.233
+#SET REMOTE PATH
+REMOTE_PATH=/home/klc/work/
+
+#SET NETWORK CARD
+NETWORK_CARD=eth0
+#SET LOCAL MOUNT PATH
+MOUNT_PATH=/mnt
+
+if [ DHCP = "1" ];then
+	udhcpc started -i $NETWORK_CARD
+else
+	#GATEWAY
+	ROUTE_IP=$IP_PRE.1
+	#SET BOARD IP
+	LOCAL_IP=$IP_PRE.234
+	#SETE MASK
+	NET_MASK=255.255.255.0
+	
+	#DOWN NETWORK CARD
+	ifconfig ${NETWORK_CARD} down
+	#SET MAC ADDRESS
+	ifconfig ${NETWORK_CARD} hw ether 12:25:34:21:65:54
+	#SET LOCAL IP
+	ifconfig ${NETWORK_CARD} ${LOCAL_IP}
+	#SET MASK
+	ifconfig ${NETWORK_CARD} netmask ${NET_MASK}
+	#ADD GATEWAY
+	route add default gw ${ROUTE_IP}
+	#OPEN LOCAL LOOP
+	ifconfig lo up
+	#OPEN NETWORK CARD
+	ifconfig ${NETWORK_CARD} up
+	sleep 3
+	ping $REMOTE_IP -c 3
+fi
+
+if [ ! -e "$MOUNT_PATH" ] ;then
+    mkdir "$MOUNT_PATH" -p
+fi
+
+#MOUNT
+mount -o nolock,wsize=1024,rsize=1024 ${REMOTE_IP}:${REMOTE_PATH} ${MOUNT_PATH}
+
+ls ${MOUNT_PATH}
