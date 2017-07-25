c0b6c453688b6d07424537ed573ec3cfc5d80091
konglingchun <konglc@idatachina.com>
2017-07-25 15:44:38 +0800

    must install git before

--- vim/vim_install.sh
+++ vim/vim_install.sh
@@ -1,6 +1,7 @@
 sudo apt-get remove vim-common
 sudo apt-get install vim vim-doc vim-scripts vim-runtime exuberant-ctags
 sudo apt-get install curl
+sudo apt-get install git
 
 curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
 cp vimrc.local ~/.vimrc.local
