aa09621b5a430a26a3d894a0089947523de79155
kong_ling_chun <kong_ling_chun@sina.com>
2018-08-21 17:49:06 +0800

    add tools

--- tools.sh
+++ tools.sh
@@ -9,8 +9,11 @@ sudo apt-get install gimp
 sudo apt-get install shutter
 sudo apt-get install octave
 sudo apt-get install clipit
+sudo apt-get install gksu
 sudo apt-get install mtd-utils
+sudo apt-get install u-boot-tools
 sudo apt-get install doxygen
+sudo apt-get install autoconf automake libtool
 sudo apt-get install meld
 sudo apt-get install ubuntu-restricted-extras
 sudo apt-get install unace p7zip-rar unar sharutils rar arj lunzip lzip
