cdd96258c71318dfa1021ac82874393fec6530a5
kong_ling_chun <kong_ling_chun@sina.com>
2018-08-21 17:47:40 +0800

    Solve the slow problem of CD command

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,2 +1,4 @@
 sudo apt-get install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#解决进入git目录卡顿的问题
+git config --global oh-my-zsh.hide-status 1
