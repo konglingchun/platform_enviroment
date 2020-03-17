18cead74a7023d10430f42b65dbe75203a278f0b
kong_ling_chun <kong_ling_chun@sina.com>
2020-03-17 16:41:05 +0800

    change gcc g++ version

--- gcc_g++_version.sh
+++ gcc_g++_version.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-	sudo apt-get install gcc-4.8
-	sudo apt-get install g++-4.8
-	sudo apt-get install gcc-4.8-multilib
-	sudo apt-get install g++-4.8-multilib
-	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
-	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50	
 	sudo apt-get install gcc-5
 	sudo apt-get install g++-5
 	sudo apt-get install gcc-5-multilib
 	sudo apt-get install g++-5-multilib
-	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 100
-	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 100
+	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 50
+	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 50
+
+	sudo apt-get install gcc-7
+	sudo apt-get install g++-7
+	sudo apt-get install gcc-7-multilib
+	sudo apt-get install g++-7-multilib
+	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 100
+	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 100
 
 #查看版本
 	#gcc -v
