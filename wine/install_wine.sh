7f15c3425a30ac58eeca27b6cb21570475c912dc
kong_ling_chun <kong_ling_chun@sina.com>
2020-05-14 17:06:18 +0800

    wine

--- wine/install_wine.sh
+++ wine/install_wine.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:ubuntu-wine/ppa
 sudo apt-get update
-sudo apt-get install wine1.6 winetricks
+sudo apt-get install wine64 winetricks
 
-sudo apt-get install ppa-purge
-sudo ppa-purge ppa:ubuntu-wine/ppa
-
-#wine remove software
-#wine uninstaller
