fa20c9d112cb04316e21d66b6e235da1d5034ae7
kong_ling_chun <kong_ling_chun@sina.com>
2023-12-14 00:05:03 +0800

    wireshark

--- install_wireshark.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install wireshark*
-sudo dpkg-reconfigure wireshark-common
-sudo usermod -a -G wireshark $USER
-#sudo reboot
--- wireshark.sh
+++ wireshark.sh
@@ -1,15 +1,22 @@
 #1.安装wireshark
-	sudo apt-get install wireshark
+sudo apt-get install -y wireshark wireshark-common wireshark-doc wireshark-dev
+#sudo apt-get install -y wireshark-gtk wireshark-qt
+
 #2.在弹出的对话中选择“是”
-	sudo dpkg-reconfigure wireshark-common
+sudo dpkg-reconfigure wireshark-common
+
 #3.将你的用户名添加到wireshark组中(配置Wireshark在非root用户下运行)
-	sudo usermod -a -G wireshark $USER
+sudo usermod -a -G wireshark $USER
+
 #4.将dumpcap的所属组改为wireshark
-	sudo chgrp wireshark /usr/bin/dumpcap
+sudo chgrp wireshark /usr/bin/dumpcap
+
 #5.改变dumpcap的权限，让wireshark组成员可以执行该程序
-	sudo chmod 750 /usr/bin/dumpcap
+sudo chmod 750 /usr/bin/dumpcap
+
 #6.使用setcap获取权限
-	sudo setcap cap_net_raw,cap_net_admin=eip /usr/bin/dumpcap
+sudo setcap cap_net_raw,cap_net_admin=eip /usr/bin/dumpcap
+
 #7.验证改变
-	sudo getcap /usr/bin/dumpcap
+sudo getcap /usr/bin/dumpcap
 
