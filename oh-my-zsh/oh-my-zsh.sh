29c7377374e5158c85a1a7a5035b3dd83bee7c6e
kong_ling_chun <kong_ling_chun@sina.com>
2023-12-13 22:58:31 +0800

    modify oh-my-zsh

--- oh-my-zsh/oh-my-zsh.sh
+++ oh-my-zsh/oh-my-zsh.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 sudo apt install -y zsh
 sudo apt install -y git
-sudo apt install curl
+sudo apt install -y curl
+
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
 cp ~/.zshrc ~/.zshrc.orig
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 chsh -s /bin/zsh
