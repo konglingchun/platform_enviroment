edd5f721cbadea8f89637923611e0160f1a5d350
kong_ling_chun <kong_ling_chun@sina.com>
2023-12-27 14:54:17 +0800

    python3

--- python.sh
+++ python.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
 #python
-python_packages_install()
+
+
+python2_packages_install()
 {
 
 	sudo apt install -y python python-pip python-tk python-dev
-	sudo apt install -y python3 python3-pip python3-tk python3-dev
-	sudo apt install -y python3.10 python3-pip python3.10-dev
 
-	sudo apt install -y python-virtualenv python3-virtualenv
-	sudo apt install -y python-protobuf python3-protobuf protobuf-compiler
-	sudo apt install -y python-markdown python3-markdown
+	sudo apt install -y python-virtualenv
+	sudo apt install -y python-protobuf protobuf-compiler
+	sudo apt install -y python-markdown
+}
+
+python3_packages_install()
+{
+	sudo apt install -y python3 python3-pip python3-tk python3-dev
+
+	sudo apt install -y python3-virtualenv
+	sudo apt install -y python3-protobuf protobuf-compiler
+	sudo apt install -y python3-markdown
+}
+
+python_packages_install()
+{
+
+	python2_packages_install
+	python3_packages_install
 }
 
 python_version_init()
@@ -40,18 +56,20 @@ python_pip_install()
 	python -m pip install Beautifulsoup4
 	python -m pip install opencv-python
 	python -m pip install recommonmark
-	python -m pip install pillow==8.0
-	python -m pip install numpy==1.19.2
-	python -m pip install matplotlib==3.3.2
-	python -m pip install scipy==1.5.2
+	python -m pip install pillow
+	python -m pip install numpy
+	python -m pip install matplotlib
+	python -m pip install scipy
 	python -m pip install serial
-	python -m pip install opencv-python==3.4.2.16
-	python -m pip install pyserial==3.5
+	python -m pip install opencv-python
+	python -m pip install pyserial
 	python -m pip install asserts
 	python -m pip install mobly
+	python -m pip install psutil
 }
 
 #python_packages_install
 #python_version_init
 #python_version_select
 #python_pip_install
+
