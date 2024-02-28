c387bb4d6c9c8a78d0f02034a708afe2c2d7c5d6
kong_ling_chun <kong_ling_chun@sina.com>
2024-02-28 15:41:06 +0800

    fio

--- tools_dev.sh
+++ tools_dev.sh
@@ -21,6 +21,7 @@ sudo apt install -y gthumb webp
 sudo apt install -y tree
 sudo apt install -y htop
 sudo apt install -y stress
+sudo apt install -y fio
 sudo apt install -y iptux
 sudo apt install -y meld patch
 sudo apt install -y curl
