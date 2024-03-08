69df6113d071752d05a9983ff1093c19ee352070
kong_ling_chun <kong_ling_chun@sina.com>
2024-03-08 20:35:03 +0800

    terminator

--- terminator/install.sh
+++ terminator/install.sh
@@ -5,3 +5,10 @@ cp terminator ~/.config -a
 #修改#!/usr/bin/python为#!/usr/bin/python2，保存
 sudo gedit `which terminator`
 
+#设置Ctrl+Alt+T的快捷键为打开gnome-terminal
+gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/gnome-terminal
+gsettings set org.gnome.desktop.default-applications.terminal exec-arg "-x"
+
+#换回默认的设置
+#gsettings reset org.gnome.desktop.default-applications.terminal exec
+#gsettings reset org.gnome.desktop.default-applications.terminal exec-arg
